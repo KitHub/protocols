cf844a2dc1c673c7a854ee27400015fa17275f80
caoxudong <caoxudong818@gmail.com>
2026-06-04 21:32:55 +0800

    mod projectgeneratorapi download api

--- projectgeneratorapi/projectgeneratorapi.validator.pb.go
+++ projectgeneratorapi/projectgeneratorapi.validator.pb.go
@@ -9,6 +9,7 @@ import (
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/mwitkow/go-proto-validators"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
+	_ "google.golang.org/genproto/googleapis/api/httpbody"
 	regexp "regexp"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
@@ -30,9 +31,6 @@ func (this *DownloadGeneratedProjectRequest) Validate() error {
 	if !(len(this.Seq) < 51) {
 		return github_com_mwitkow_go_proto_validators.FieldError("Seq", fmt.Errorf(`value '%v' must have a length smaller than '51'`, this.Seq))
 	}
-	return nil
-}
-func (this *DownloadGeneratedProjectResponse) Validate() error {
 	return nil
 }
 func (this *GenerateProjectRequestComponent) Validate() error {
